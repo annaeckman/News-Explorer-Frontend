e05c25aa21553b706a0f82f4601e8139330a8844
annaeckman <annaeckman@icloud.com>
2024-10-08 10:22:00 -0700

    newCardLists displaying 3 at a time

--- src/components/NewsCardList/NewsCardList.jsx
+++ src/components/NewsCardList/NewsCardList.jsx
@@ -1,8 +1,22 @@
 import "../NewsCardList/NewsCardList.css";
 import NewsCard from "../NewsCard/NewsCard";
+import { useState } from "react";
+
+// initial empty state, no search term, hidden
+// happy path: news data has length, show results and show more
+// no results for search term: "no results please try again"
+// wrapper component that differentiates btwn these 3, then render's each
+// of the above 3 options
+
+// i need to hide the button when all 100 items are shown
 
 function NewsCardList({ newsData }) {
-  const activeNewsDataItems = newsData.slice(0, 3);
+  const [activeNewsDataLength, setActiveNewsDataLength] = useState(3);
+  const activeNewsDataItems = newsData.slice(0, activeNewsDataLength);
+
+  const handleOnClick = () => {
+    setActiveNewsDataLength((prevState) => prevState + 3);
+  };
 
   return (
     <section className="news-cards-list">
@@ -14,8 +28,19 @@ function NewsCardList({ newsData }) {
           ))}
         </ul>
       </div>
+      <button onClick={handleOnClick} className="news-cards-list__more-button">
+        Show more
+      </button>
     </section>
   );
 }
 
 export default NewsCardList;
+
+// important lessons:
+// don't unneccessarily create a state variable for something you can derive
+// you want to store as specific, as small a piece of state you can,
+// that's used to derive what you need that's coming through from props
+// avoid setting state from state - it will cause a loop!
+// if you HAVE TO, you have to use a useEffect
+// 1st rule of useEffects is don't use them. lol.
