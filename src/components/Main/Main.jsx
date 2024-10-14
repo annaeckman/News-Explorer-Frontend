6cab52c6c900d19e9cd91972eb554dfac9efdd5f
annaeckman <annaeckman@icloud.com>
2024-10-14 10:08:26 -0700

    moved date logic into utils file

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,6 +5,16 @@ import NewsCardsList from "../NewsCardList/NewsCardList";
 import { useState } from "react";
 import { getNews } from "../../utils/newsapi";
 import { APIkey } from "../../utils/constants";
+import {
+  todaysDate,
+  currentYear,
+  currentMonth,
+  currentDay,
+  lastWeeksDate,
+  lastWeekYear,
+  lastWeekMonth,
+  lastWeekDay,
+} from "../../utils/Dates";
 
 function Main({ handleLoginClick }) {
   const [newsData, setNewsData] = useState([]);
@@ -19,19 +29,19 @@ function Main({ handleLoginClick }) {
   };
 
   const handleSearchSubmit = () => {
-    const todaysDate = new Date();
-    const currentYear = todaysDate.getFullYear();
-    const currentMonth = todaysDate.getMonth() + 1;
-    const currentDay = todaysDate.getDate();
+    // const todaysDate = new Date();
+    // const currentYear = todaysDate.getFullYear();
+    // const currentMonth = todaysDate.getMonth() + 1;
+    // const currentDay = todaysDate.getDate();
 
     const to = `${currentYear}-${currentMonth}-${currentDay}`;
 
-    const lastWeeksDate = new Date(
-      todaysDate.getTime() - 7 * 24 * 60 * 60 * 1000
-    );
-    const lastWeekYear = lastWeeksDate.getFullYear();
-    const lastWeekMonth = lastWeeksDate.getMonth() + 1;
-    const lastWeekDay = lastWeeksDate.getDate();
+    // const lastWeeksDate = new Date(
+    //   todaysDate.getTime() - 7 * 24 * 60 * 60 * 1000
+    // );
+    // const lastWeekYear = lastWeeksDate.getFullYear();
+    // const lastWeekMonth = lastWeeksDate.getMonth() + 1;
+    // const lastWeekDay = lastWeeksDate.getDate();
     const from = `${lastWeekYear}-${lastWeekMonth}-${lastWeekDay}`;
 
     setIsLoading(true);
