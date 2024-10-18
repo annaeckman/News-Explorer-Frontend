794af14872855d741eeb28476c23667fd326dbdc
annaeckman <annaeckman@icloud.com>
2024-10-17 19:32:31 -0700

    fixing hamburger menu

--- src/components/HamburgerMenu/HamburgerMenu.jsx
+++ src/components/HamburgerMenu/HamburgerMenu.jsx
@@ -1,9 +1,11 @@
 import "./HamburgerMenu.css";
-import NavLoggedIn from "../NavLoggedIn/NavLoggedIn";
-import NavLoggedOut from "../NavLoggedOut/NavLoggedOut";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
 function HamburgerMenu({ isLoggedIn, onClose, handleLoginClick, isInverse }) {
-  console.log(isLoggedIn);
+  const { currentUser } = useContext(CurrentUserContext);
+
   return (
     <div className="hamburger-menu">
       <div className="hamburger-menu__top">
@@ -17,9 +19,45 @@ function HamburgerMenu({ isLoggedIn, onClose, handleLoginClick, isInverse }) {
 
       <div className="hamburger-menu__links">
         {isLoggedIn ? (
-          <NavLoggedIn isInverse={isInverse} />
+          <ul className="hamburger-menu__list">
+            <li className="hamburger-menu-item">
+              <Link to="/" className="hamburger-menu__link">
+                Home
+              </Link>
+            </li>
+            <li className="hamburger-menu-item">
+              <Link to="/saved-news" className="hamburger-menu__link">
+                Saved Articles
+              </Link>
+            </li>
+            <li className="hamburger-menu-item">
+              <div className="hamburger-menu__signout-div">
+                <p className="hamburger-menu-signout-name">
+                  Elise{currentUser.name}
+                </p>
+                <button
+                  onClick={handleLogout}
+                  className={signoutBtnClass}
+                ></button>
+              </div>
+            </li>
+          </ul>
         ) : (
-          <NavLoggedOut handleLoginClick={handleLoginClick} />
+          <ul className="hamburger-menu__links">
+            <li className="hamburger-menu-item">
+              <Link to="/" className="hamburger-menu__link">
+                Home
+              </Link>
+            </li>
+            <li>
+              <button
+                onClick={handleLoginClick}
+                className="hamburger-menu__signin-btn"
+              >
+                Sign in
+              </button>
+            </li>
+          </ul>
         )}
       </div>
     </div>
