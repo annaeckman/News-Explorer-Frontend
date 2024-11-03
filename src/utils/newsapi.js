c25880f6fe50f1b919f69cf007fb729ea6277e1e
annaeckman <annaeckman@icloud.com>
2024-11-03 11:04:56 -0800

    added api key back into params

--- src/utils/newsapi.js
+++ src/utils/newsapi.js
@@ -1,14 +1,12 @@
 import { processServerResponse } from "../utils/utils";
 import stubData from "./stubResponse.json";
 const baseUrl = import.meta.env.VITE_NEWS_API_BASE_URL;
-console.log(import.meta.env);
 
 export const getNews = (q, apiKey, from, to) => {
   // return new Promise((r) => r(stubData));
-  return fetch(`${baseUrl}?q=${q}&from=${from}&to=${to}&pageSize=100`, {
-    method: "GET",
-    headers: {
-      authorization: import.meta.env.VITE_NEWS_API_KEY,
-    },
-  }).then(processServerResponse);
+  return fetch(
+    `${baseUrl}?q=${q}&from=${from}&to=${to}&pageSize=100&apiKey=${
+      import.meta.env.VITE_NEWS_API_KEY
+    }`
+  ).then(processServerResponse);
 };
